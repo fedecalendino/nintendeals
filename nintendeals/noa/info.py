c0f8aef6c529f92e31a9fd0f16a9174b35200029
fedecalendino <fede@calendino.com>
2020-10-22 10:34:26 +0200

    Added slug to game_info in NA

--- nintendeals/noa/info.py
+++ nintendeals/noa/info.py
@@ -6,7 +6,7 @@ from nintendeals.noa.scrapers import nintendo
 from nintendeals.noa.util import build_game
 
 
-def game_info(nsuid: str) -> Optional[Game]:
+def game_info_by_nsuid(nsuid: str) -> Optional[Game]:
     data = algolia.search_by_nsuid(nsuid)
 
     if not data:
@@ -15,3 +15,30 @@ def game_info(nsuid: str) -> Optional[Game]:
     data["extra"] = nintendo.scrap(data["slug"])
 
     return build_game(data)
+
+
+def game_info_by_slug(slug: str) -> Optional[Game]:
+    extra = nintendo.scrap(slug)
+    nsuid = extra.get("nsuid")
+
+    if not nsuid:
+        return None
+
+    data = algolia.search_by_nsuid(nsuid)
+
+    if not data:
+        return None
+
+    data["extra"] = extra
+
+    return build_game(data)
+
+
+def game_info(nsuid: str = None, slug: str = None) -> Optional[Game]:
+    if nsuid:
+        return game_info_by_nsuid(nsuid)
+
+    if slug:
+        return game_info_by_slug(slug)
+
+    return None
