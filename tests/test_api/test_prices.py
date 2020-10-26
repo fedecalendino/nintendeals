d6a63f3f5cfa9181656eae1205ce6e1e3ab893a6
fedecalendino <fede@calendino.com>
2020-10-26 02:01:40 +0100

    Add mocked test for prices

--- tests/test_api/test_prices.py
+++ tests/test_api/test_prices.py
@@ -1,8 +1,12 @@
-from unittest import TestCase
+from unittest import mock, TestCase
 
+import ddt
+
+from nintendeals import noa
 from nintendeals.api import prices
 
 
+@ddt.ddt
 class TestPrices(TestCase):
 
     def test_fetch_prices(self):
@@ -48,3 +52,69 @@ class TestPrices(TestCase):
             "The amount of nsuids must between 1 and 50.",
             str(context.exception)
         )
+
+    @ddt.data(("70010000000025", False), ("70010000000529", True))
+    @ddt.unpack
+    def test_sales(self, nsuid, on_sale):
+        class MockedResponse:
+            status_code = 200
+
+            def json(self):
+                return {
+                    "personalized": False,
+                    "country": "US",
+                    "prices": [
+                        {
+                            "title_id": 70010000000025,
+                            "sales_status": "onsale",
+                            "regular_price": {
+                                "amount": "$59.99",
+                                "currency": "USD",
+                                "raw_value": "59.99"
+                            },
+                        },
+                        {
+                            "title_id": 70010000000529,
+                            "sales_status": "onsale",
+                            "regular_price": {
+                                "amount": "$59.99",
+                                "currency": "USD",
+                                "raw_value": "59.99"
+                            },
+                            "discount_price": {
+                                "amount": "$41.99",
+                                "currency": "USD",
+                                "raw_value": "41.99",
+                                "start_datetime": "2020-04-30T06:00:00Z",
+                                "end_datetime": "2020-05-10T14:59:59Z"
+                            }
+                        }
+                    ]
+                }
+
+            def raise_for_status(self):
+                pass
+
+        game = noa.game_info(nsuid=nsuid)
+
+        with mock.patch("requests.get") as patched:
+            patched.return_value = MockedResponse()
+            price = game.price(country="US")
+
+        self.assertEqual(game.nsuid, price.nsuid)
+        self.assertFalse(price.is_free_to_play)
+
+        self.assertEqual("US", price.country)
+        self.assertEqual("USD", price.currency)
+        self.assertEqual(59.99, price.value)
+
+        if not on_sale:
+            self.assertEqual("USD 59.99", str(price))
+            self.assertEqual(None, price.sale_value)
+            self.assertEqual(0, price.sale_discount)
+            self.assertFalse(price.is_sale_active)
+        else:
+            self.assertEqual("USD 41.99*", str(price))
+            self.assertEqual(41.99, price.sale_value)
+            self.assertEqual(70, price.sale_discount)
+            self.assertFalse(price.is_sale_active)
